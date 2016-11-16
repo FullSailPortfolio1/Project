9b2c7908871418ff9c2dffa58caa618fbc2f9b93
Michael  Sewell <mikesewell01@gmail.com>
2016-11-15 21:20:55 -0500

    added happy maids, also testing git hub and console

---
+++ SewellMichael_Portfolio1/HappyMaids/HappyMaids/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HappyMaids
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lvRoomList = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(275, 436);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(142, 31);
+            this.txtWidth.TabIndex = 2;
+            this.txtWidth.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(275, 365);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(142, 31);
+            this.txtLength.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 510);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Complete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Full Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Room length (inches)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 439);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(207, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Room width (inches)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(266, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(443, 42);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Welcome to Happy Maids";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(731, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Complete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 588);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 43);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Clear Form";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lvRoomList
+            // 
+            this.lvRoomList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvRoomList.Location = new System.Drawing.Point(641, 354);
+            this.lvRoomList.Name = "lvRoomList";
+            this.lvRoomList.Size = new System.Drawing.Size(300, 383);
+            this.lvRoomList.TabIndex = 13;
+            this.lvRoomList.UseCompatibleStateImageBehavior = false;
+            this.lvRoomList.View = System.Windows.Forms.View.List;
+            this.lvRoomList.SelectedIndexChanged += new System.EventHandler(this.lvRoomList_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(508, 366);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(108, 70);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add >";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(508, 442);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(108, 73);
+            this.btnRemove.TabIndex = 15;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Square Feet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1000, 819);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lvRoomList);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Happy Maids";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView lvRoomList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ SewellMichael_Portfolio1/HappyMaids/HappyMaids/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HappyMaids
+{
+    public partial class Form1 : Form
+    {
+        public class Room
+        {
+            public string Name;
+            public double Width;
+            public double length;
+            public double SquareFeet;
+        }
+        private List<Room> Rooms = new List<Room>();
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //name text box
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // number of rooms text box
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //room length text bo
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string userName = textBox1.Text.Trim();
+            MessageBox.Show("Hello "+ userName + "! Welcome to the Happy Maids price quoting application.\r\n                                         Press OK to contuniue.");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            txtWidth.Clear();
+            txtLength.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Double length;
+            Double width;
+            if (!System.Double.TryParse(txtLength.Text, out length) || !System.Double.TryParse(txtWidth.Text, out width)) return;
+            Room newroom = new Room();
+            newroom.length = length;
+            newroom.Width = width;
+            newroom.SquareFeet = width * length;
+            Rooms.Add(newroom);
+            UpdateRoomList();
+        }
+        private void UpdateRoomList()
+        {
+            lvRoomList.Clear();
+            foreach(Room temproom in Rooms)
+            {
+                lvRoomList.Items.Add(temproom.SquareFeet + " sqft");
+            }
+        }
+
+        private void lvRoomList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lvRoomList.SelectedIndices.Count <= 0) return;
+            Rooms.RemoveAt(lvRoomList.SelectedIndices[0]);
+            UpdateRoomList();
+        }
+        private double GetTotalSqFeet()
+        {
+            double ret = 0;
+            foreach (Room temproom in Rooms)
+            {
+                ret += temproom.SquareFeet;
+            }
+            return ret;
+        }
+
+    }
+}
